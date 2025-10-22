e493047a467b85a6d01a2200e1cea79d6c9fe8a0
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2025-10-21 23:44:45 -0700

    Balanced saturation numbers in existing foods

--- src/main/java/net/lunarluned/mortem/item/ModFoods.java
+++ src/main/java/net/lunarluned/mortem/item/ModFoods.java
@@ -13,25 +13,25 @@ public class ModFoods {
 
     public static final FoodProperties CRIMSON_STEW = (new FoodProperties.Builder().nutrition(4).saturationModifier(.7F).build());
     public static final FoodProperties WARPED_STEW = (new FoodProperties.Builder().nutrition(3).saturationModifier(.8F).build());
-    public static final FoodProperties FUNGAL_STEW = (new FoodProperties.Builder().nutrition(6).saturationModifier(.65F).build());
+    public static final FoodProperties FUNGAL_STEW = (new FoodProperties.Builder().nutrition(6).saturationModifier(.55F).build());
     public static final FoodProperties MUSHROOM_STEW_TUSK = (new FoodProperties.Builder().nutrition(3).saturationModifier(.9F).build());
-    public static final FoodProperties SCRAMBLED_EGGS_TUSK = (new FoodProperties.Builder().nutrition(3).saturationModifier(1F).build());
+    public static final FoodProperties SCRAMBLED_EGGS_TUSK = (new FoodProperties.Builder().nutrition(5).saturationModifier(.65F).build());
 
     public static final FoodProperties HARD_BOILED_EGG = (new FoodProperties.Builder().nutrition(2).saturationModifier(.5f).build());
-    public static final FoodProperties SCRAMBLED_EGGS = (new FoodProperties.Builder().nutrition(6).saturationModifier(.65F).build());
+    public static final FoodProperties SCRAMBLED_EGGS = (new FoodProperties.Builder().nutrition(6).saturationModifier(.45F).build());
 
-    public static final FoodProperties SUSHI_ROLL = (new FoodProperties.Builder().nutrition(4).saturationModifier(.5F).build());
+    public static final FoodProperties SUSHI_ROLL = (new FoodProperties.Builder().nutrition(4).saturationModifier(.25F).build());
 
     // Overworld Foods
 
-    public static final FoodProperties SWEET_POTATO = (new FoodProperties.Builder().nutrition(3).saturationModifier(2f).build());
+    public static final FoodProperties SWEET_POTATO = (new FoodProperties.Builder().nutrition(3).saturationModifier(1.5f).build());
     public static final FoodProperties RAW_BACON = (new FoodProperties.Builder().nutrition(2).saturationModifier(0.1f).build());
-    public static final FoodProperties COOKED_BACON = (new FoodProperties.Builder().nutrition(3).saturationModifier(0.75f).build());
+    public static final FoodProperties COOKED_BACON = (new FoodProperties.Builder().nutrition(3).saturationModifier(0.25f).build());
     public static final FoodProperties BEEF_PATTY = (new FoodProperties.Builder().nutrition(3).saturationModifier(0.0f).build());
-    public static final FoodProperties COOKED_BEEF_PATTY = (new FoodProperties.Builder().nutrition(4).saturationModifier(.75f).build());
+    public static final FoodProperties COOKED_BEEF_PATTY = (new FoodProperties.Builder().nutrition(4).saturationModifier(.25f).build());
     public static final FoodProperties BURGER = (new FoodProperties.Builder().nutrition(8).saturationModifier(.65f).build());
-    public static final FoodProperties BACON_BURGER = (new FoodProperties.Builder().nutrition(10).saturationModifier(1.05f).build());
-    public static final FoodProperties TOAST = (new FoodProperties.Builder().nutrition(2).saturationModifier(.5f).build());
+    public static final FoodProperties BACON_BURGER = (new FoodProperties.Builder().nutrition(10).saturationModifier(0.7f).build());
+    public static final FoodProperties TOAST = (new FoodProperties.Builder().nutrition(2).saturationModifier(.25f).build());
     public static final FoodProperties TOASTED_BREAD = (new FoodProperties.Builder().nutrition(5).saturationModifier(0.5f).build());
 /*
     public static final FoodProperties SWEET_BERRY_JAM = (new FoodProperties.Builder().nutrition(6).saturationMod(3f).fast().effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 0), 1F).build());
