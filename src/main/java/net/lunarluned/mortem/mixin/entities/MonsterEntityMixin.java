477e0671d503b9deb1dc126d23193f1af0f25b69
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2025-11-09 02:17:57 -0800

    Increased Follow Range of Monster mobs

---
+++ src/main/java/net/lunarluned/mortem/mixin/entities/MonsterEntityMixin.java
@@ -0,0 +1,26 @@
+package net.lunarluned.mortem.mixin.entities;
+
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.monster.Zombie;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Zombie.class)
+public abstract class MonsterEntityMixin {
+
+    @Inject(method = "createAttributes", at = @At("RETURN"), cancellable = true)
+    private static void modifyFollowRange(CallbackInfoReturnable<AttributeSupplier.Builder> cir) {
+        AttributeSupplier.Builder builder = cir.getReturnValue();
+
+        // Get existing follow range and double it
+        builder.add(Attributes.FOLLOW_RANGE, 64);
+
+        // Set the modified builder back
+        cir.setReturnValue(builder);
+    }
+}
