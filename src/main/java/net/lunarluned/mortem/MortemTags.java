2f59973f0d9e5a017021fecb702a8a83c10ba6d3
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2025-11-09 02:17:09 -0800

    Campfires can be lit by torches to prepare for unlit place state, and sets players who stand on them on fire

--- src/main/java/net/lunarluned/mortem/MortemTags.java
+++ src/main/java/net/lunarluned/mortem/MortemTags.java
@@ -3,8 +3,15 @@ package net.lunarluned.mortem;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class MortemTags {
     public static final TagKey<Block> ARROW_BREAKABLE = TagKey.create(Registries.BLOCK, ResourceLocation.tryBuild("mortem", "arrow_breakable"));
+
+    public static final TagKey<EntityType<?>> ABLE_TO_BE_ZOMBIFIED = TagKey.create(Registries.ENTITY_TYPE, ResourceLocation.tryBuild("mortem", "able_to_be_zombified"));
+
+    public static final TagKey<Item> TORCHES = TagKey.create(Registries.ITEM, ResourceLocation.tryBuild("mortem", "torches"));
 }
---
+++ src/main/java/net/lunarluned/mortem/mixin/blocks/CampfireBlockMixin.java
@@ -0,0 +1,52 @@
+package net.lunarluned.mortem.mixin.blocks;
+
+import net.lunarluned.mortem.MortemTags;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.InsideBlockEffectApplier;
+import net.minecraft.world.entity.InsideBlockEffectType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.CampfireBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CampfireBlock.class)
+public class CampfireBlockMixin {
+    @Inject(method = "useItemOn", at = @At("HEAD"), cancellable = true)
+    private void mortem_campfireLightFromTorch(ItemStack itemStack, BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+        ItemStack stack = player.getItemInHand(interactionHand);
+        if (stack.is(MortemTags.TORCHES)) {
+            if (!level.isClientSide()) {
+                // set campfire lit
+                if (blockState.hasProperty(CampfireBlock.LIT) && !blockState.getValue(CampfireBlock.LIT)) {
+                    level.playSound(null, blockPos, SoundEvents.FIRECHARGE_USE, SoundSource.BLOCKS, 1.0F, 1.0F);
+                    level.setBlock(blockPos, blockState.setValue(CampfireBlock.LIT, true), 3);
+                }
+            }
+            cir.setReturnValue(InteractionResult.SUCCESS);
+        }
+    }
+
+    @Inject(method = "entityInside", at = @At("TAIL"))
+    private void mortem_setFireEntityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity, InsideBlockEffectApplier insideBlockEffectApplier, CallbackInfo ci) {
+        if (blockState.hasProperty(CampfireBlock.LIT) && blockState.getValue(CampfireBlock.LIT)) {
+            insideBlockEffectApplier.apply(InsideBlockEffectType.CLEAR_FREEZE);
+            insideBlockEffectApplier.apply(InsideBlockEffectType.FIRE_IGNITE);
+            insideBlockEffectApplier.runAfter(InsideBlockEffectType.FIRE_IGNITE, (entityx) -> entityx.hurt(entityx.level().damageSources().inFire(), 1));
+        }
+    }
+    }
