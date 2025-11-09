53898d46329c26c96434d2206838681790dd1736
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2025-11-09 10:59:08 -0800

    Campfires spread fire and spawn unlit now, added Campfire Flammable Blocks tag

--- src/main/java/net/lunarluned/mortem/MortemTags.java
+++ src/main/java/net/lunarluned/mortem/MortemTags.java
@@ -10,6 +10,8 @@ import net.minecraft.world.level.block.Block;
 
 public class MortemTags {
     public static final TagKey<Block> ARROW_BREAKABLE = TagKey.create(Registries.BLOCK, ResourceLocation.tryBuild("mortem", "arrow_breakable"));
+    public static final TagKey<Block> FLAMMABLE_BLOCKS = TagKey.create(Registries.BLOCK, ResourceLocation.tryBuild("mortem", "flammable_blocks"));
+
 
     public static final TagKey<EntityType<?>> ABLE_TO_BE_ZOMBIFIED = TagKey.create(Registries.ENTITY_TYPE, ResourceLocation.tryBuild("mortem", "able_to_be_zombified"));
 
