ade6029513cd8141426aa57e506736ae0e4dfdc0
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2025-11-09 18:04:12 -0800

    More tags

--- src/main/java/net/lunarluned/mortem/MortemTags.java
+++ src/main/java/net/lunarluned/mortem/MortemTags.java
@@ -3,7 +3,6 @@ package net.lunarluned.mortem;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -14,6 +13,8 @@ public class MortemTags {
 
 
     public static final TagKey<EntityType<?>> ABLE_TO_BE_ZOMBIFIED = TagKey.create(Registries.ENTITY_TYPE, ResourceLocation.tryBuild("mortem", "able_to_be_zombified"));
+    public static final TagKey<EntityType<?>> FUNGUS_IMMUNE = TagKey.create(Registries.ENTITY_TYPE, ResourceLocation.tryBuild("mortem", "fungus_immune"));
+
 
     public static final TagKey<Item> TORCHES = TagKey.create(Registries.ITEM, ResourceLocation.tryBuild("mortem", "torches"));
 }
