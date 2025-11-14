a3836ce429d023af5a1d6e15af0047372ed94895
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2025-11-14 00:11:43 -0800

    Slightly nerfed Regen chance after buffing it

--- src/main/java/net/lunarluned/mortem/mixin/entities/LivingEntityNaturalRegenMixin.java
+++ src/main/java/net/lunarluned/mortem/mixin/entities/LivingEntityNaturalRegenMixin.java
@@ -2,7 +2,6 @@ package net.lunarluned.mortem.mixin.entities;
 
 import net.lunarluned.mortem.effect.ModEffects;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.LivingEntity;
@@ -23,7 +22,7 @@ public abstract class LivingEntityNaturalRegenMixin {
 
     @Shadow @Nullable public abstract LivingEntity asLivingEntity();
 
-    @Unique public int regenerationChance = 35;
+    @Unique public int regenerationChance = 25;
 
     @Inject(method = "heal", at = @At("HEAD"), cancellable = true)
     private void mortem_redirectHealForNaturalRegen(float amount, CallbackInfo ci) {
@@ -53,13 +52,13 @@ public abstract class LivingEntityNaturalRegenMixin {
                 ServerLevel level = player.level() instanceof ServerLevel ? (ServerLevel) player.level() : null;
                 if (level != null
                         && level.getGameRules().getBoolean(GameRules.RULE_NATURAL_REGENERATION)
-                        // player must have enough hunger to regen
+                        // more checks and allat
                         && player.getFoodData().getFoodLevel() <= 18
-                        // must actually be damaged
                         && player.getHealth() < player.getMaxHealth()
                         && !player.isSpectator()
                         && !player.isCreative()
-                ) if (!(regenerationChance == 0)) {
+                        // regen time slowed
+                ) if (!(regenerationChance == 0) && player.tickCount % 100 == 0) {
                     float g = player.getHealth();
                     int randomValue = Mth.nextInt(RandomSource.create(), 1, 100);
                     if (randomValue < regenerationChance) {
