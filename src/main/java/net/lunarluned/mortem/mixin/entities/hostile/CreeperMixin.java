e4b40c2c75df52c6d94dfbdd0558d12f6742d669
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2025-12-04 19:57:59 -0800

    Creepers are invulnerable to explosion damage now

--- src/main/java/net/lunarluned/mortem/mixin/entities/hostile/CreeperMixin.java
+++ src/main/java/net/lunarluned/mortem/mixin/entities/hostile/CreeperMixin.java
@@ -1,15 +1,33 @@
 package net.lunarluned.mortem.mixin.entities.hostile;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Creeper.class)
-public abstract class CreeperMixin {
+public abstract class CreeperMixin extends Monster {
+
+    protected CreeperMixin(EntityType<? extends Monster> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Override
+    public boolean isInvulnerableTo(ServerLevel serverLevel, DamageSource damageSource) {
+        if (damageSource.is(DamageTypeTags.IS_EXPLOSION)) {
+            return true;
+        }
+        return super.isInvulnerableTo(serverLevel, damageSource);
+    }
 
     @Inject(method = "createAttributes", at = @At("RETURN"), cancellable = true)
     private static void modifyFollowRange(CallbackInfoReturnable<AttributeSupplier.Builder> cir) {
