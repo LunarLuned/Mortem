f3474d22f4b1d1f3abe7133eabda461f98c95f48
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2025-12-04 19:59:12 -0800

    Spiders nolonger take fall damage & can now place cobwebs when hitting victims

--- src/main/java/net/lunarluned/mortem/mixin/entities/hostile/SpiderMixin.java
+++ src/main/java/net/lunarluned/mortem/mixin/entities/hostile/SpiderMixin.java
@@ -1,16 +1,49 @@
 package net.lunarluned.mortem.mixin.entities.hostile;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Spider;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Spider.class)
-public abstract class SpiderMixin {
+public abstract class SpiderMixin extends Monster {
+
+    protected SpiderMixin(EntityType<? extends Monster> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Override
+    public boolean doHurtTarget(ServerLevel serverLevel, Entity entity) {
+        if (super.doHurtTarget(serverLevel, entity)) {
+            if (entity instanceof LivingEntity && entity.getRandom().nextInt(9) > 6) {
+                BlockPos pos = entity.getOnPos().above();
+                if (this.level().getBlockState(pos).isAir())
+                    this.level().setBlockAndUpdate(pos, Blocks.COBWEB.defaultBlockState());
+            }
+            }
+        return false;
+    }
+
+    @Override
+    public boolean causeFallDamage(double d, float f, DamageSource damageSource) {
+        return false;
+    }
 
     @Inject(method = "createAttributes", at = @At("RETURN"), cancellable = true)
     private static void modifyFollowRange(CallbackInfoReturnable<AttributeSupplier.Builder> cir) {
