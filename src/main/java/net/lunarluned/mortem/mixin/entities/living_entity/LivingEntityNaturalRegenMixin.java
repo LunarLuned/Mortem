d5082a930d4921c21933794a0dc08b61935cadde
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2026-05-20 21:06:47 -0700

    Fixed not being able to heal at all oh m y god

--- src/main/java/net/lunarluned/mortem/mixin/entities/living_entity/LivingEntityNaturalRegenMixin.java
+++ src/main/java/net/lunarluned/mortem/mixin/entities/living_entity/LivingEntityNaturalRegenMixin.java
@@ -4,6 +4,7 @@ import net.lunarluned.mortem.effect.ModEffects;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.gamerules.GameRules;
@@ -22,7 +23,7 @@ public abstract class LivingEntityNaturalRegenMixin {
 
     @Shadow @Nullable public abstract LivingEntity asLivingEntity();
 
-    @Unique public int regenerationChance = 10;
+    @Unique public int regenerationChance = 15;
 
     @Inject(method = "heal", at = @At("HEAD"), cancellable = true)
     private void mortem_redirectHealForNaturalRegen(float amount, CallbackInfo ci) {
@@ -50,17 +51,12 @@ public abstract class LivingEntityNaturalRegenMixin {
             }
                 // Server level and gamerule check
                 ServerLevel level = player.level() instanceof ServerLevel ? (ServerLevel) player.level() : null;
-                if (level != null
-                        && level.getGameRules().get(GameRules.NATURAL_HEALTH_REGENERATION)
-                        // more checks and allat
-                        && player.getHealth() < player.getMaxHealth()
-                        && !player.isSpectator()
-                        && !player.isCreative()
-                        // regen time slowed
-                ) if (!(regenerationChance == 0) && player.tickCount % 100 == 0) {
+                if (level != null && player.getHealth() < player.getMaxHealth() && !player.isSpectator() && player.getFoodData().getFoodLevel() <= 10 && !player.isCreative())
+                    if (player.getFoodData().getFoodLevel() <= 10 || player.hasEffect(MobEffects.INSTANT_HEALTH) || player.getEffect(MobEffects.REGENERATION).getAmplifier() > 0)
+                    if (!(regenerationChance == 0) && player.tickCount % 100 == 0) {
                     float g = player.getHealth();
                     int randomValue = Mth.nextInt(RandomSource.create(), 1, 100);
-                    if (randomValue < regenerationChance) {
+                    if (randomValue < regenerationChance || player.hasEffect(MobEffects.INSTANT_HEALTH) || player.getEffect(MobEffects.REGENERATION).getAmplifier() > 0) {
                         if (g > 0.0F) {
                             player.setHealth(g + amount);
                         }
