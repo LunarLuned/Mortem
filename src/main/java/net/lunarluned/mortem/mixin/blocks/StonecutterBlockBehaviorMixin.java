cc80640a5b80e2f14e9a8a41649a7f6d9a8cddbf
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2026-05-20 19:07:21 -0700

    Fixed Saw Table damage range

--- src/main/java/net/lunarluned/mortem/mixin/blocks/StonecutterBlockBehaviorMixin.java
+++ src/main/java/net/lunarluned/mortem/mixin/blocks/StonecutterBlockBehaviorMixin.java
@@ -19,6 +19,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.StonecutterBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -40,26 +41,26 @@ public class StonecutterBlockBehaviorMixin {
 
         Direction facing = state.getValue(StonecutterBlock.FACING);
 
-        Vec3 blockCenter = Vec3.atCenterOf(pos);
-        Vec3 entityPos = entity.position();
+        AABB bladeBox;
 
-        Vec3 toEntity = entityPos.subtract(blockCenter);
+        double thickness = 0.25D;
 
-        Vec3 facingVec = new Vec3(
-                facing.getStepX(),
-                facing.getStepY(),
-                facing.getStepZ()
-        );
+        if (facing.getAxis() == Direction.Axis.X) {
+            bladeBox = new AABB(pos.getX() + 0.5D - thickness, pos.getY(), pos.getZ(), pos.getX() + 0.5D + thickness, pos.getY() + 1.0D, pos.getZ() + 1.0D);
+        } else {
+            bladeBox = new AABB(pos.getX(), pos.getY(), pos.getZ() + 0.5D - thickness, pos.getX() + 1.0D, pos.getY() + 1.0D, pos.getZ() + 0.5D + thickness);
+        }
 
-        double dot = toEntity.normalize().dot(facingVec);
+        if (entity.getBoundingBox().intersects(bladeBox)) {
 
-        if (dot > 0.3) {
-            if (entity.tickCount % 10 == 0 && !entity.isInvulnerable() && !(entity instanceof ItemEntity)) {
-                level.playSound(entity, pos, SoundEvents.UI_STONECUTTER_TAKE_RESULT, SoundSource.BLOCKS, 1, 1);
+            entity.makeStuckInBlock(state, new Vec3(0.75, 0.8, 0.75));
+
+            if (entity.tickCount % 10 == 0 && !(entity instanceof ItemEntity) && !entity.isInvulnerable()) {
+
+                level.playSound(entity, pos, SoundEvents.UI_STONECUTTER_TAKE_RESULT, SoundSource.BLOCKS, 1.0F, 1.0F);
                 entity.hurt(level.damageSources().generic(), 2.0F);
                 entity.invulnerableTime = 0;
             }
-            entity.makeStuckInBlock(state, new Vec3(0.75, 0.8, 0.75));
         }
     }
 }
